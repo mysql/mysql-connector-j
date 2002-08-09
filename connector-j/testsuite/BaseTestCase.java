6a668d00539476be01b70613e20fe113025a87d0
mark_matthews <>
2002-08-09 15:23:23 +0000

    Added copyright notice.

--- connector-j/testsuite/BaseTestCase.java
+++ connector-j/testsuite/BaseTestCase.java
@@ -1,13 +1,23 @@
-package testsuite;
+/*
+ Copyright (C) 2002 MySQL AB
 
-/**
- * @author Administrator
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   
  */
+ 
+package testsuite;
 
 import java.sql.Connection;
 import java.sql.SQLException;
