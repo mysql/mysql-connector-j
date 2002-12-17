b817b6ba05aaabf316bee7c4e0f72a1e43047b5e
mmatthew <>
2002-12-17 21:00:20 +0000

    Code Cleanup.

--- connector-j/com/mysql/jdbc/RowData.java
+++ connector-j/com/mysql/jdbc/RowData.java
@@ -16,123 +16,178 @@
       Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
       
  */
- 
 package com.mysql.jdbc;
 
 import java.sql.SQLException;
 
-/** 
+
+/**
  * This interface abstracts away how row data is accessed by
  * the result set. It is meant to allow a static implementation
  * (Current version), and a streaming one.
+ *
+ * @author dgan
  */
 public interface RowData {
-
-    //~ Instance/static variables .............................................
-
-    /** 
+    /**
      * What's returned for the size of a result set
      * when its size can not be determined.
      */
     public static final int RESULT_SET_SIZE_UNKNOWN = -1;
 
-    //~ Methods ...............................................................
+    /** 
+     * Returns true if we got the last element.
+     *
+     * @return true if after last row 
+     * @throws SQLException if a database error occurs
+     */
+    boolean isAfterLast() throws SQLException;
 
-    /** Returns true if another row exsists.*/
-    boolean hasNext()
-                    throws SQLException;
+    /** 
+     * Only works on non dynamic result sets.
+     *
+     * @param index row number to get at
+     * @return row data at index
+     * @throws SQLException if a database error occurs
+     */
+    byte[][] getAt(int index) throws SQLException;
 
-    /** Returns the next row.*/
-    byte[][] next()
-                  throws SQLException;
+    /** 
+     * Returns if iteration has not occured yet.
+     *
+     * @return true if before first row
+     * @throws SQLException if a database error occurs
+     */
+    boolean isBeforeFirst() throws SQLException;
 
-    /** Returns if iteration has not occured yet.*/
-    boolean isBeforeFirst()
-                          throws SQLException;
+    /**
+     * Moves the current position in the result set to
+     * the given row number.
+     * 
+     * @param rowNumber row to move to
+     * @throws SQLException if a database error occurs
+     */
+    void setCurrentRow(int rowNumber) throws SQLException;
 
-    /** Returns true if we got the last element.*/
-    boolean isAfterLast()
-                        throws SQLException;
+    /**
+     * Returns the current position in the result set as
+     * a row number.
+     * 
+     * @return the current row number
+     * @throws SQLException if a database error occurs
+     */
+    int getCurrentRowNumber() throws SQLException;
 
-    /** Moves to before first.*/
-    void beforeFirst()
-                     throws SQLException;
+    /**
+     * Returns true if the result set is dynamic.
+     *
+     * This means that move back and move forward won't work
+     * because we do not hold on to the records.
+     * 
+     * @return true if this result set is streaming from the server
+     * @throws SQLException if a database error occurs
+     */
+    boolean isDynamic() throws SQLException;
 
-    /** Moves to after last.*/
-    void afterLast()
-                   throws SQLException;
-
-    /** Moves to before last so next el is the last el.*/
-    void beforeLast()
-                    throws SQLException;
+    /** 
+     * Has no records.
+     * 
+     * @return true if no records
+     * @throws SQLException if a database error occurs
+     */
+    boolean isEmpty() throws SQLException;
 
     /**
      * Are we on the first row of the result set?
+     * 
+     * @return true if on first row
+     * @throws SQLException if a database error occurs
      */
-    boolean isFirst()
-                    throws SQLException;
+    boolean isFirst() throws SQLException;
 
     /**
      * Are we on the last row of the result set?
+     * 
+     * @return true if on last row
+     * @throws SQLException if a database error occurs
      */
-    boolean isLast()
-                   throws SQLException;
-
-    /** We're done.*/
-    void close()
-               throws SQLException;
+    boolean isLast() throws SQLException;
 
     /**
-     * Returns the current position in the result set as 
-     * a row number.
+     * Adds a row to this row data.
+     *
+     * @param row the row to add
+     * @throws SQLException if a database error occurs
      */
-    int getCurrentRowNumber()
-                            throws SQLException;
 
-    /**
-     * Moves the current position in the result set to 
-     * the given row number.
+    void addRow(byte[][] row) throws SQLException;
+
+    /** 
+     * Moves to after last.
+     *
+     * @throws SQLException if a database error occurs
      */
-    void setCurrentRow(int rowNumber)
-                       throws SQLException;
+    void afterLast() throws SQLException;
+
+    /** 
+     * Moves to before first.
+     *
+     * @throws SQLException if a database error occurs
+     */
+    void beforeFirst() throws SQLException;
+
+    /** 
+     * Moves to before last so next el is the last el.
+     *
+     * @throws SQLException if a database error occurs
+     */
+    void beforeLast() throws SQLException;
+
+    /** 
+     * We're done.
+     *
+     * @throws SQLException if a database error occurs
+     */
+    void close() throws SQLException;
+
+    /** 
+     * Returns true if another row exsists.
+     *
+     * @return true if more rows
+     * @throws SQLException if a database error occurs
+     */
+    boolean hasNext() throws SQLException;
 
     /**
      * Moves the current position relative 'rows' from
      * the current position.
+     *
+     * @param rows the relative number of rows to move
+     * @throws SQLException if a database error occurs
      */
-    void moveRowRelative(int rows)
-                         throws SQLException;
+    void moveRowRelative(int rows) throws SQLException;
 
     /** 
-     * Returns true if the result set is dynamic.
-     *  
-     * This means that move back and move forward won't work
-     * because we do not hold on to the records.
+     * Returns the next row.
+     *
+     * @return the next row value
+     * @throws SQLException if a database error occurs
      */
-    boolean isDynamic()
-                      throws SQLException;
-
-    /** Only works on non dynamic result sets.*/
-    int size()
-             throws SQLException;
-
-    /** Has no records.*/
-    boolean isEmpty()
-                    throws SQLException;
-
-    /** Only works on non dynamic result sets.*/
-    byte[][] getAt(int index)
-                   throws SQLException;
-
-    /**
-     * Adds a row to this row data.
-     */
-    void addRow(byte[][] row)
-                throws SQLException;
+    byte[][] next() throws SQLException;
 
     /**
      * Removes the row at the given index.
+     *
+     * @param index the row to move to
+     * @throws SQLException if a database error occurs
      */
-    void removeRow(int index)
-                   throws SQLException;
+    void removeRow(int index) throws SQLException;
+
+    /** 
+     * Only works on non dynamic result sets.
+     *
+     * @return the size of this row data
+     * @throws SQLException if a database error occurs
+     */
+    int size() throws SQLException;
 }
