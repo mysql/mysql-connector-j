cc0c5e8680e59f418e3b9c8eb3d8648fb99a8300
mmatthew <>
2002-12-17 17:04:46 +0000

    Removed unused imports

--- connector-j/com/mysql/jdbc/Blob.java
+++ connector-j/com/mysql/jdbc/Blob.java
@@ -19,10 +19,7 @@
 package com.mysql.jdbc;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-
 import java.sql.SQLException;
 
 
