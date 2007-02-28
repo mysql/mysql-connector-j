d235c2601df161073c442dba95f8a49d7e18188f
mmatthews <>
2007-02-28 22:45:19 +0000

    Fixed issue where XADataSources couldn't be bound into JNDI, 	  as the DataSourceFactory didn't know how to create instances 	  of them. (merged from 5.0)

--- connector-j/src/testsuite/simple/DataSourceTest.java
+++ connector-j/src/testsuite/simple/DataSourceTest.java
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2002-2004 MySQL AB
+ Copyright (C) 2002-2007 MySQL AB
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of version 2 of the GNU General Public License as 
@@ -202,7 +202,7 @@ public class DataSourceTest extends BaseTestCase {
 			pooledConnection.getConnection().close();
 		}
 	}
-	
+
 	/**
 	 * Tests whether XADataSources can be bound into JNDI
 	 * 
