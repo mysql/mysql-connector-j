e8c0310fe758028bc5316bc6ec8ba5789d38a459
mmatthews <>
2007-08-29 17:01:19 +0000

    Updated/added copyright headers.

--- connector-j/src/com/mysql/jdbc/ConnectionPropertiesImpl.java
+++ connector-j/src/com/mysql/jdbc/ConnectionPropertiesImpl.java
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2002-2004 MySQL AB
+ Copyright (C) 2002-2007 MySQL AB
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of version 2 of the GNU General Public License as 
