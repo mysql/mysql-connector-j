8b9b2633c891841fcce3bef2e4cd784eba2e5a97
Tonci Grgin <tonci.grgin@oracle.com>
2012-03-15 18:01:51 +0100

    Fix for Bug#64621, setMaxRows was not correctly processed during CS PS metadata collection causing entire resultset to be fetched and possibly leading to OOM.

--- src/com/mysql/jdbc/PreparedStatement.java
+++ src/com/mysql/jdbc/PreparedStatement.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2002, 2011, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2002, 2012, Oracle and/or its affiliates. All rights reserved.
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
@@ -2898,7 +2898,7 @@ public class PreparedStatement extends com.mysql.jdbc.StatementImpl implements
 				mdStmt = new PreparedStatement(this.connection,
 						this.originalSql, this.currentCatalog, this.parseInfo);
 
-				mdStmt.setMaxRows(0);
+				mdStmt.setMaxRows(1);
 
 				int paramCount = this.parameterValues.length;
 
