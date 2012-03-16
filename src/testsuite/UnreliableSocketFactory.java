fa5d54a57391904da30b504ef8b59ff615413280
Tonci Grgin <tonci.grgin@oracle.com>
2012-03-16 09:49:21 +0100

    Legal notices, no code change.

--- src/testsuite/UnreliableSocketFactory.java
+++ src/testsuite/UnreliableSocketFactory.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ Copyright (c) 2008, 2011, Oracle and/or its affiliates. All rights reserved.
  
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
--- src/testsuite/regression/EscapeProcessorRegressionTest.java
+++ src/testsuite/regression/EscapeProcessorRegressionTest.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ Copyright (c) 2005, 2012, Oracle and/or its affiliates. All rights reserved.
  
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
