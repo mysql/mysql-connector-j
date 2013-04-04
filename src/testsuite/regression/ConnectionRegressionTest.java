ecb8f10b7a011648a41a4050106fbee89fc88a3f
Alexander Soklakov <alexander.soklakov@oracle.com>
2013-04-04 17:14:30 +0400

    Fix for ConnectionRegressionTest.testBug64205, localized messages.

--- src/testsuite/regression/ConnectionRegressionTest.java
+++ src/testsuite/regression/ConnectionRegressionTest.java
@@ -4066,10 +4066,10 @@ public class ConnectionRegressionTest extends BaseTestCase {
 					testRs = testSt.executeQuery("SELECT * FROM `"+dbname+"`.`ほげほげ`");
 				} catch (SQLException e2) {
 					if (e2 instanceof MySQLSyntaxErrorException) {
-						assertEquals("Таблица '"+dbname+".ほげほげ' не существует", e2.getLocalizedMessage());
+						assertEquals("Таблица '"+dbname+".ほげほげ' не существует", e2.getMessage());
 					} else if (e2.getErrorCode() == MysqlErrorNumbers.ER_FILE_NOT_FOUND) {
 						// this could happen on Windows with 5.5 and 5.6 servers where BUG#14642248 exists
-						assertTrue("File not found error message should be russian but is this one: "+e2.getMessage()+" (localized message: "+e2.getLocalizedMessage()+").", e2.getLocalizedMessage().toLowerCase().contains("файл"));
+						assertTrue("File not found error message should be russian but is this one: "+e2.getMessage(), e2.getMessage().contains("файл"));
 					} else {
 						throw e2;
 					}
