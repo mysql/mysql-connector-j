f18cba9e0a789e53d9c1a5514d66b8be7cb94b85
Jess Balint <jess.balint@oracle.com>
2013-12-03 17:03:38 -0600

    Fix incorrect license headers

--- src/com/mysql/jdbc/ReflectiveStatementInterceptorAdapter.java
+++ src/com/mysql/jdbc/ReflectiveStatementInterceptorAdapter.java
@@ -1,22 +1,26 @@
 /*
- Copyright (c) 2009, 2012, Oracle and/or its affiliates. All rights reserved., Inc. All rights reserved.
- U.S. Government Rights - Commercial software. Government users are subject
- to the Sun Microsystems, Inc. standard license agreement and applicable
- provisions of the FAR and its supplements. Use is subject to license terms.
- This distribution may include materials developed by third parties.Sun,
- Sun Microsystems, the Sun logo and MySQL Enterprise Monitor are
- trademarks or registered trademarks of Sun Microsystems, Inc. in the U.S.
- and other countries.
+ Copyright (c) 2009, 2012, Oracle and/or its affiliates. All rights reserved.
+ 
 
- Copyright (c) 2009, 2010, Oracle and/or its affiliates. All rights reserved., Inc. Tous droits réservés.
- L'utilisation est soumise aux termes du contrat de licence.Cette
- distribution peut comprendre des composants développés par des tierces
- parties.Sun, Sun Microsystems,  le logo Sun et  MySQL Enterprise Monitor sont
- des marques de fabrique ou des marques déposées de Sun Microsystems, Inc.
- aux Etats-Unis et dans du'autres pays.
+  The MySQL Connector/J is licensed under the terms of the GPLv2
+  <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
+  There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+  this software, see the FLOSS License Exception
+  <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
 
- */
+  This program is free software; you can redistribute it and/or modify it under the terms
+  of the GNU General Public License as published by the Free Software Foundation; version 2
+  of the License.
 
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+  Floor, Boston, MA 02110-1301  USA
+
+*/
 package com.mysql.jdbc;
 
 import java.lang.reflect.InvocationTargetException;
