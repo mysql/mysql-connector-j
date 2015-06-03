ec0c97cc8c66ff783a0729c7fb76f985a2d863a8
Jess Balint <jess.balint@oracle.com>
2015-06-02 20:49:40 -0500

    MYSQLCONNJ-44 Add message writer

---
+++ src/com/mysql/cj/mysqlx/MessageWriter.java
@@ -0,0 +1,96 @@
+/*
+  Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+
+  The MySQL Connector/J is licensed under the terms of the GPLv2
+  <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
+  There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+  this software, see the FLOSS License Exception
+  <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+
+  This program is free software; you can redistribute it and/or modify it under the terms
+  of the GNU General Public License as published by the Free Software Foundation; version 2
+  of the License.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+  Floor, Boston, MA 02110-1301  USA
+
+ */
+
+package com.mysql.cj.mysqlx;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.protobuf.MessageLite;
+
+import static com.mysql.cj.mysqlx.protobuf.Mysqlx.ClientMessages;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxAdmin.CommandExecute;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxCrud.Delete;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxCrud.Find;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxCrud.Insert;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxCrud.Update;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxSession.AuthenticateStart;
+import com.mysql.cj.core.exception.WrongArgumentException;
+
+/**
+ * Low-level message writer for protobuf messages.
+ */
+public class MessageWriter {
+    /**
+     * Header length of MySQL-X packet.
+     */
+    static final int HEADER_LEN = 5;
+
+    /**
+     * Store a mapping of message class to "ClientMessages" type tag. This is used to generate the header when sending a message.
+     */
+    private static Map<Class, Integer> messageClassToClientMessageType = new HashMap<>();
+
+    static {
+        messageClassToClientMessageType.put(AuthenticateStart.class, ClientMessages.Type.SESS_AUTHENTICATE_START_VALUE);
+        messageClassToClientMessageType.put(CommandExecute.class, ClientMessages.Type.ADMIN_COMMAND_EXECUTE_VALUE);
+        messageClassToClientMessageType.put(Delete.class, ClientMessages.Type.CRUD_DELETE_VALUE);
+        messageClassToClientMessageType.put(Find.class, ClientMessages.Type.CRUD_FIND_VALUE);
+        messageClassToClientMessageType.put(Insert.class, ClientMessages.Type.CRUD_INSERT_VALUE);
+        messageClassToClientMessageType.put(Update.class, ClientMessages.Type.CRUD_UPDATE_VALUE);
+    }
+
+    private BufferedOutputStream outputStream;
+
+    public MessageWriter(BufferedOutputStream os) {
+        this.outputStream = os;
+    }
+
+    /**
+     * Looking the type tag for a protobuf message class.
+     */
+    private static int getTypeForMessageClass(Class msgClass) {
+        Integer tag = messageClassToClientMessageType.get(msgClass);
+        if (tag == null) {
+            throw new WrongArgumentException("Invalid message class " + msgClass.getName());
+        }
+        return tag;
+    }
+
+    /**
+     * Send a message.
+     */
+    public void write(MessageLite msg) throws IOException {
+        int type = getTypeForMessageClass(msg.getClass());
+        int size = HEADER_LEN + msg.getSerializedSize();
+        byte[] sizeHeader = ByteBuffer.allocate(4).putInt(size).array();
+        this.outputStream.write(sizeHeader);
+        this.outputStream.write(type);
+        msg.writeTo(this.outputStream);
+        this.outputStream.flush();
+    }
+}
---
+++ unittests/com/mysql/cj/mysqlx/MessageWriterTest.java
@@ -0,0 +1,87 @@
+/*
+  Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+
+  The MySQL Connector/J is licensed under the terms of the GPLv2
+  <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
+  There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
+  this software, see the FLOSS License Exception
+  <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+
+  This program is free software; you can redistribute it and/or modify it under the terms
+  of the GNU General Public License as published by the Free Software Foundation; version 2
+  of the License.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with this
+  program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
+  Floor, Boston, MA 02110-1301  USA
+
+ */
+
+package com.mysql.cj.mysqlx;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.google.protobuf.ByteString;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.mysql.cj.mysqlx.protobuf.Mysqlx.ClientMessages;
+import static com.mysql.cj.mysqlx.protobuf.Mysqlx.Ok;
+import static com.mysql.cj.mysqlx.protobuf.MysqlxSession.AuthenticateStart;
+import com.mysql.cj.core.exception.WrongArgumentException;
+
+public class MessageWriterTest {
+    private ByteArrayOutputStream outputStream;
+    private MessageWriter writer;
+
+    @Before
+    public void setUp() {
+        this.outputStream = new ByteArrayOutputStream();
+        this.writer = new MessageWriter(new BufferedOutputStream(this.outputStream));
+    }
+
+    /**
+     * Test that we can (properly) write a complete message.
+     */
+    @Test
+    public void testCompleteWriteMessage() throws Exception {
+        // construct and write the message
+        AuthenticateStart.Builder msgBuilder = AuthenticateStart.newBuilder();
+        msgBuilder.setMechName("Unit-Test");
+        msgBuilder.setAuthData(ByteString.copyFromUtf8("some-auth-data"));
+        AuthenticateStart msg = msgBuilder.build();
+        this.writer.write(msg);
+
+        // verify the written packet
+        byte[] sentBytes = this.outputStream.toByteArray();
+        int msgSize = msg.getSerializedSize();
+        int totalSize = msgSize + MessageWriter.HEADER_LEN;
+        assertTrue("Required for rest of test, should never fail", msgSize < Byte.MAX_VALUE);
+        // message size (4 bytes big endian)
+        assertEquals(0, sentBytes[0]);
+        assertEquals(0, sentBytes[1]);
+        assertEquals(0, sentBytes[2]);
+        assertEquals(totalSize, sentBytes[3]);
+        assertEquals("Type tag", ClientMessages.Type.SESS_AUTHENTICATE_START_VALUE, sentBytes[4]);
+        assertEquals("Entire packet size should be header bytes + serialized message", totalSize, sentBytes.length);
+    }
+
+    @Test
+    public void testBadMessageClass() throws Exception {
+        try {
+            // try sending "Ok" which is a server-sent message. should fail with exception
+            this.writer.write(Ok.getDefaultInstance());
+            fail("Writing OK message should fail");
+        } catch (WrongArgumentException ex) {
+        }
+    }
+}
