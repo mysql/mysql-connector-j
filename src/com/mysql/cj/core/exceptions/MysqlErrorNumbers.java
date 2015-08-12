9f85f5c1e27699e80c8ac7ce7e2bf6a124244074
Jess Balint <jess.balint@oracle.com>
2015-08-12 17:28:41 -0500

    Add xplugin error numbers

--- src/com/mysql/cj/core/exceptions/MysqlErrorNumbers.java
+++ src/com/mysql/cj/core/exceptions/MysqlErrorNumbers.java
@@ -943,6 +943,44 @@ public final class MysqlErrorNumbers {
     public final static int ER_DUP_LIST_ENTRY = 1909; //SQLSTATE: HY000 Message: Duplicate entry '%s'.; was introduced in 5.7.4.
     public final static int ER_SQL_MODE_NO_EFFECT = 1910; //SQLSTATE: HY000 Message: '%s' mode no longer has any effect. Use STRICT_ALL_TABLES or STRICT_TRANS_TABLES instead.; was introduced in 5.7.4. 
 
+    public static final int ER_X_SERVICE_ERROR = 5010;
+    public static final int ER_X_SESSION = 5011;
+    public static final int ER_X_INVALID_ARGUMENT = 5012;
+    public static final int ER_X_MISSING_ARGUMENT = 5013;
+    public static final int ER_X_BAD_INSERT_DATA = 5014;
+    public static final int ER_X_CMD_NUM_ARGUMENTS = 5015;
+    public static final int ER_X_CMD_ARGUMENT_TYPE = 5016;
+    public static final int ER_X_CMD_ARGUMENT_VALUE = 5017;
+    public static final int ER_X_BAD_UPDATE_DATA = 5050;
+    public static final int ER_X_BAD_TYPE_OF_UPDATE = 5051;
+    public static final int ER_X_BAD_COLUMN_TO_UPDATE = 5052;
+    public static final int ER_X_BAD_MEMBER_TO_UPDATE = 5053;
+    public static final int ER_X_BAD_STATEMENT_ID = 5110;
+    public static final int ER_X_BAD_CURSOR_ID = 5111;
+    public static final int ER_X_BAD_SCHEMA = 5112;
+    public static final int ER_X_BAD_TABLE = 5113;
+    public static final int ER_X_BAD_PROJECTION = 5114;
+    public static final int ER_X_DOC_ID_MISSING = 5115;
+    public static final int ER_X_DOC_ID_DUPLICATE = 5116;
+    public static final int ER_X_DOC_REQUIRED_FIELD_MISSING = 5117;
+    public static final int ER_X_PROJ_BAD_KEY_NAME = 5120;
+    public static final int ER_X_BAD_DOC_PATH = 5121;
+    public static final int ER_X_CURSOR_EXISTS = 5122;
+    public static final int ER_X_EXPR_BAD_OPERATOR = 5150;
+    public static final int ER_X_EXPR_BAD_NUM_ARGS = 5151;
+    public static final int ER_X_EXPR_MISSING_ARG = 5152;
+    public static final int ER_X_EXPR_BAD_TYPE_VALUE = 5153;
+    public static final int ER_X_EXPR_BAD_VALUE = 5154;
+    public static final int ER_X_INVALID_COLLECTION = 5156;
+    public static final int ER_X_INVALID_ADMIN_COMMAND = 5157;
+    public static final int ER_X_EXPECT_NOT_OPEN = 5158;
+    public static final int ER_X_EXPECT_FAILED = 5159;
+    public static final int ER_X_EXPECT_BAD_CONDITION = 5160;
+    public static final int ER_X_EXPECT_BAD_CONDITION_VALUE = 5161;
+    public static final int ER_X_INVALID_NAMESPACE = 5162;
+    public static final int ER_X_BAD_NOTICE = 5163;
+    public static final int ER_X_CANNOT_DISABLE_NOTICE = 5164;
+
     private MysqlErrorNumbers() {
         // prevent instantiation
     }
