3c5d35284b02e25fe9aa1c2d95f658e221eb38e5
Alexander Soklakov <alexander.soklakov@oracle.com>
2015-10-02 12:01:14 +0400

    MYSQLCONNJ-622, fixed package-info

--- src/generated/java/com/mysql/cj/mysqlx/protobuf/package-info.java
+++ src/generated/java/com/mysql/cj/mysqlx/protobuf/package-info.java
@@ -24,7 +24,7 @@
 /**
  * Message classes used for serialization with Google Protocol Buffers to support MySQL-X protocol. Classes are generated using <i>protoc</i> like so:
  * <pre>
- * protoc --java_out=src/main/java -I/PATH/TO/MYSQL/plugin/x/protocol /PATH/TO/MYSQL/plugin/x/protocol/*.proto
+ * protoc --java_out=src/generated/java -I/PATH/TO/MYSQL/plugin/x/protocol /PATH/TO/MYSQL/plugin/x/protocol/*.proto
  * </pre>
  *
  * @since 6.0
