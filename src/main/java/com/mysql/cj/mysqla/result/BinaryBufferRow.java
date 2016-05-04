ed694012682e47799a3eeaca6564ecae383d8d64
Alexander Soklakov <alexander.soklakov@oracle.com>
2016-05-04 11:00:36 +0400

    MYSQLCONNJ-742, ResultSetRow should implement com.mysql.cj.api.result.Row interface

--- src/main/java/com/mysql/cj/mysqla/result/BinaryBufferRow.java
+++ src/main/java/com/mysql/cj/mysqla/result/BinaryBufferRow.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2007, 2016, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
@@ -31,25 +31,20 @@ import com.mysql.cj.api.mysqla.io.NativeProtocol.StringLengthDataType;
 import com.mysql.cj.api.mysqla.io.NativeProtocol.StringSelfDataType;
 import com.mysql.cj.api.mysqla.io.PacketPayload;
 import com.mysql.cj.core.Messages;
-import com.mysql.cj.core.exceptions.CJOperationNotSupportedException;
 import com.mysql.cj.core.exceptions.ExceptionFactory;
 import com.mysql.cj.core.result.Field;
 import com.mysql.cj.mysqla.MysqlaConstants;
 import com.mysql.cj.mysqla.MysqlaUtils;
 
 /**
- * A ResultSetRow implementation that holds one row packet (which is re-used by the driver, and thus saves memory allocations), and tries when possible to avoid
- * allocations to break out the results as individual byte[]s.
+ * A BufferRow implementation that holds one row packet from a server-side prepared statement (which is re-used by the driver,
+ * and thus saves memory allocations), and tries when possible to avoid allocations to break out the results as individual byte[]s.
+ * Rows from a server-side prepared statement are encoded differently, so we have different ways of finding where each column is, and
+ * unpacking them.
  * 
  * (this isn't possible when doing things like reading floating point values).
  */
-public class BufferRow extends ResultSetRow {
-    private PacketPayload rowFromServer;
-
-    /**
-     * The beginning of the row packet
-     */
-    private int homePosition = 0;
+public class BinaryBufferRow extends AbstractBufferRow {
 
     /**
      * The home position before the is-null bitmask for server-side prepared statement result sets
@@ -57,38 +52,14 @@ public class BufferRow extends ResultSetRow {
     private int preNullBitmaskHomePosition = 0;
 
     /**
-     * The last-requested index, used as an optimization, if you ask for the same index, we won't seek to find it. If you ask for an index that is >
-     * than the last one requested, we start seeking from the last requested index.
-     */
-    private int lastRequestedIndex = -1;
-
-    /**
-     * The position of the last-requested index, optimization in concert with lastRequestedIndex.
-     */
-    private int lastRequestedPos;
-
-    /**
-     * The metadata of the fields of this result set.
-     */
-    private Field[] metadata;
-
-    /**
-     * Is this a row from a server-side prepared statement? If so, they're encoded differently, so we have different ways of finding where each column is, and
-     * unpacking them.
-     */
-    private boolean isBinaryEncoded;
-
-    /**
      * If binary-encoded, the NULL status of each column is at the beginning of the row, so we
      */
     private boolean[] isNull;
 
-    public BufferRow(PacketPayload buf, Field[] fields, boolean isBinaryEncoded, ExceptionInterceptor exceptionInterceptor, ValueDecoder valueDecoder) {
+    public BinaryBufferRow(PacketPayload buf, Field[] fields, ExceptionInterceptor exceptionInterceptor, ValueDecoder valueDecoder) {
         super(exceptionInterceptor);
 
         this.rowFromServer = buf;
-        this.metadata = fields;
-        this.isBinaryEncoded = isBinaryEncoded;
         this.homePosition = this.rowFromServer.getPosition();
         this.preNullBitmaskHomePosition = this.homePosition;
         this.valueDecoder = valueDecoder;
@@ -98,51 +69,8 @@ public class BufferRow extends ResultSetRow {
         }
     }
 
-    private int findAndSeekToOffset(int index) {
-        if (!this.isBinaryEncoded) {
-
-            if (index == 0) {
-                this.lastRequestedIndex = 0;
-                this.lastRequestedPos = this.homePosition;
-                this.rowFromServer.setPosition(this.homePosition);
-
-                return 0;
-            }
-
-            if (index == this.lastRequestedIndex) {
-                this.rowFromServer.setPosition(this.lastRequestedPos);
-
-                return this.lastRequestedPos;
-            }
-
-            int startingIndex = 0;
-
-            if (index > this.lastRequestedIndex) {
-                if (this.lastRequestedIndex >= 0) {
-                    startingIndex = this.lastRequestedIndex;
-                } else {
-                    startingIndex = 0;
-                }
-
-                this.rowFromServer.setPosition(this.lastRequestedPos);
-            } else {
-                this.rowFromServer.setPosition(this.homePosition);
-            }
-
-            for (int i = startingIndex; i < index; i++) {
-                skipLenencBytes(this.rowFromServer);
-            }
-
-            this.lastRequestedIndex = index;
-            this.lastRequestedPos = this.rowFromServer.getPosition();
-
-            return this.lastRequestedPos;
-        }
-
-        return findAndSeekToOffsetForBinaryEncoding(index);
-    }
-
-    private int findAndSeekToOffsetForBinaryEncoding(int index) {
+    @Override
+    protected int findAndSeekToOffset(int index) {
         if (index == 0) {
             this.lastRequestedIndex = 0;
             this.lastRequestedPos = this.homePosition;
@@ -183,7 +111,7 @@ public class BufferRow extends ResultSetRow {
             if (type != MysqlaConstants.FIELD_TYPE_NULL) {
                 int length = MysqlaUtils.getBinaryEncodedLength(this.metadata[i].getMysqlTypeId());
                 if (length == 0) {
-                    skipLenencBytes(this.rowFromServer);
+                    this.rowFromServer.skipBytes(StringSelfDataType.STRING_LENENC);
                 } else if (length == -1) {
                     throw ExceptionFactory.createException(Messages.getString("MysqlIO.97") + type + Messages.getString("MysqlIO.98") + (i + 1)
                             + Messages.getString("MysqlIO.99") + this.metadata.length + Messages.getString("MysqlIO.100"), this.exceptionInterceptor);
@@ -200,20 +128,9 @@ public class BufferRow extends ResultSetRow {
         return this.lastRequestedPos;
     }
 
-    private void skipLenencBytes(PacketPayload packet) {
-        long len = packet.readInteger(IntegerDataType.INT_LENENC);
-        if (len != PacketPayload.NULL_LENGTH && len != 0) {
-            packet.setPosition(packet.getPosition() + (int) len);
-        }
-    }
-
     @Override
     public byte[] getColumnValue(int index) {
         findAndSeekToOffset(index);
-
-        if (!this.isBinaryEncoded) {
-            return this.rowFromServer.readBytes(StringSelfDataType.STRING_LENENC);
-        }
 
         if (this.getNull(index)) {
             return null;
@@ -241,43 +158,19 @@ public class BufferRow extends ResultSetRow {
         }
     }
 
+    /**
+     * Check whether a column is NULL and update the 'wasNull' status.
+     */
     @Override
-    public boolean isNull(int index) {
-        if (!this.isBinaryEncoded) {
-            findAndSeekToOffset(index);
-
-            return this.rowFromServer.readInteger(IntegerDataType.INT_LENENC) == PacketPayload.NULL_LENGTH;
-        }
-
-        return this.isNull[index];
-    }
-
-    @Override
-    public long length(int index) {
-        findAndSeekToOffset(index);
-
-        long length = this.rowFromServer.readInteger(IntegerDataType.INT_LENENC);
-
-        if (length == PacketPayload.NULL_LENGTH) {
-            return 0;
-        }
-
-        return length;
-    }
-
-    @Override
-    public void setColumnValue(int index, byte[] value) {
-        throw ExceptionFactory.createException(CJOperationNotSupportedException.class, Messages.getString("OperationNotSupportedException.0"));
+    public boolean getNull(int columnIndex) {
+        this.wasNull = this.isNull[columnIndex];
+        return this.wasNull;
     }
 
     @Override
     public ResultSetRow setMetadata(Field[] f) {
         super.setMetadata(f);
-
-        if (this.isBinaryEncoded) {
-            setupIsNullBitmask();
-        }
-
+        setupIsNullBitmask();
         return this;
     }
 
@@ -323,23 +216,16 @@ public class BufferRow extends ResultSetRow {
     public <T> T getValue(int columnIndex, ValueFactory<T> vf) {
         findAndSeekToOffset(columnIndex);
 
-        int length;
-        if (this.isBinaryEncoded) {
-            // field length is type-specific in binary-encoded results
-            int type = this.metadata[columnIndex].getMysqlTypeId();
-            length = MysqlaUtils.getBinaryEncodedLength(type);
-            if (length == 0) {
-                length = (int) this.rowFromServer.readInteger(IntegerDataType.INT_LENENC);
-            } else if (length == -1) {
-                throw ExceptionFactory.createException(Messages.getString("MysqlIO.97") + type + Messages.getString("MysqlIO.98") + (columnIndex + 1)
-                        + Messages.getString("MysqlIO.99") + this.metadata.length + Messages.getString("MysqlIO.100"), this.exceptionInterceptor);
-            }
-        } else {
+        // field length is type-specific in binary-encoded results
+        int type = this.metadata[columnIndex].getMysqlTypeId();
+        int length = MysqlaUtils.getBinaryEncodedLength(type);
+        if (length == 0) {
             length = (int) this.rowFromServer.readInteger(IntegerDataType.INT_LENENC);
+        } else if (length == -1) {
+            throw ExceptionFactory.createException(Messages.getString("MysqlIO.97") + type + Messages.getString("MysqlIO.98") + (columnIndex + 1)
+                    + Messages.getString("MysqlIO.99") + this.metadata.length + Messages.getString("MysqlIO.100"), this.exceptionInterceptor);
         }
-        // MUST get offset after we read the length, i.e. don't reverse order of these two statements
-        int offset = this.rowFromServer.getPosition();
 
-        return getValueFromBytes(columnIndex, this.rowFromServer.getByteBuffer(), offset, length, vf);
+        return getValueFromBytes(columnIndex, this.rowFromServer.getByteBuffer(), this.rowFromServer.getPosition(), length, vf);
     }
 }
