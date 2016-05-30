d21e7e3b720496f355d3aa80bd13e902b66dba08
Alexander Soklakov <alexander.soklakov@oracle.com>
2016-05-30 23:03:11 +0400

    Changed the location of commercial README and LICENSE.

--- src/main/java/com/mysql/cj/core/LicenseConfiguration.java
+++ src/main/java/com/mysql/cj/core/LicenseConfiguration.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2002, 2015, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2002, 2016, Oracle and/or its affiliates. All rights reserved.
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
@@ -44,7 +44,7 @@ public class LicenseConfiguration {
      *             if commercial license is required, but not found
      */
     public static void checkLicenseType(Map<String, String> serverVariables) {
-        // This is a GPL build, so we don't check anything...
+        // we don't check anything by default
     }
 
     private LicenseConfiguration() {
