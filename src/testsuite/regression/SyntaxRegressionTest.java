6c33f8a06fd4c8204d26d5fc7cd77576b9d0d205
Filipe Silva <filipe.silva@oracle.com>
2017-01-09 23:42:00 +0000

    Simple code cleanup in SyntaxRegressionTest.java.

--- src/testsuite/regression/SyntaxRegressionTest.java
+++ src/testsuite/regression/SyntaxRegressionTest.java
@@ -40,8 +40,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
