e94bdbe2f2bdabdb73eb6f8e98914e015c4c8241
Filipe Silva <filipe.silva@oracle.com>
2017-01-13 23:21:04 +0000

    Fix for Bug#81108 (23264511), FabricMySQLConnectionProxy.setShardTable method logic miss.

--- src/com/mysql/fabric/jdbc/FabricMySQLConnectionProxy.java
+++ src/com/mysql/fabric/jdbc/FabricMySQLConnectionProxy.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
 
   The MySQL Connector/J is licensed under the terms of the GPLv2
   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most MySQL Connectors.
@@ -337,8 +337,8 @@ public class FabricMySQLConnectionProxy extends ConnectionPropertiesImpl impleme
             String db = this.database;
             if (shardTable.contains(".")) {
                 String pair[] = shardTable.split("\\.");
-                table = pair[0];
-                db = pair[1];
+                db = pair[0];
+                table = pair[1];
             }
             this.shardMapping = this.fabricConnection.getShardMapping(db, table);
             if (this.shardMapping == null) {
